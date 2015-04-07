139d12861bb4004e5c134281b9cc813f60c0ebb5
HuajieYang <huajie@pdx.edu>
2015-04-07 03:32:49 +0000

    Map OHAS trips destination taz and summarize the results of LQ analysis based on emp and sizeterms, and TDM trips.

--- code/ExploreLQ.R
+++ code/ExploreLQ.R
@@ -10,52 +10,52 @@ listOMX(in.file_tripdistribution)
 
   # calculate hbw trips 
   hbwhighIncDist <- readMatrixOMX(in.file_tripdistribution, "hbwmidIncDist")
-  hbwhighIncDist_rowsum <- rowSums(hbwhighIncDist)
+  hbwhighIncDist_colsum <- colSums(hbwhighIncDist)
 
   hbwmidIncDist <- readMatrixOMX(in.file_tripdistribution, "hbwmidIncDist")
-  hbwmidIncDist_rowsum <- rowSums(hbwmidIncDist)
+  hbwmidIncDist_colsum <- colSums(hbwmidIncDist)
 
   hbwlowIncDist <- readMatrixOMX(in.file_tripdistribution, "hbwlowIncDist")
-  hbwlowIncDist_rowsum <- rowSums(hbwlowIncDist)
+  hbwlowIncDist_colsum <- colSums(hbwlowIncDist)
   
-  hbwtrips <- hbwhighIncDist_rowsum + hbwmidIncDist_rowsum + hbwlowIncDist_rowsum
+  hbwtrips <- hbwhighIncDist_colsum + hbwmidIncDist_colsum + hbwlowIncDist_colsum
   
   
   # calculate hbs trips 
   hbshighIncDist <- readMatrixOMX(in.file_tripdistribution, "hbsmidIncDist")
-  hbshighIncDist_rowsum <- rowSums(hbshighIncDist)
+  hbshighIncDist_colsum <- colSums(hbshighIncDist)
   
   hbsmidIncDist <- readMatrixOMX(in.file_tripdistribution, "hbsmidIncDist")
-  hbsmidIncDist_rowsum <- rowSums(hbsmidIncDist)
+  hbsmidIncDist_colsum <- colSums(hbsmidIncDist)
   
   hbslowIncDist <- readMatrixOMX(in.file_tripdistribution, "hbslowIncDist")
-  hbslowIncDist_rowsum <- rowSums(hbslowIncDist)
+  hbslowIncDist_colsum <- colSums(hbslowIncDist)
   
-  hbstrips <- hbshighIncDist_rowsum + hbsmidIncDist_rowsum + hbslowIncDist_rowsum
+  hbstrips <- hbshighIncDist_colsum + hbsmidIncDist_colsum + hbslowIncDist_colsum
   
   # calculate hbr trips 
   hbrhighIncDist <- readMatrixOMX(in.file_tripdistribution, "hbrmidIncDist")
-  hbrhighIncDist_rowsum <- rowSums(hbrhighIncDist)
+  hbrhighIncDist_colsum <- colSums(hbrhighIncDist)
   
   hbrmidIncDist <- readMatrixOMX(in.file_tripdistribution, "hbrmidIncDist")
-  hbrmidIncDist_rowsum <- rowSums(hbrmidIncDist)
+  hbrmidIncDist_colsum <- colSums(hbrmidIncDist)
   
   hbrlowIncDist <- readMatrixOMX(in.file_tripdistribution, "hbrlowIncDist")
-  hbrlowIncDist_rowsum <- rowSums(hbrlowIncDist)
+  hbrlowIncDist_colsum <- colSums(hbrlowIncDist)
   
-  hbrtrips <- hbrhighIncDist_rowsum + hbrmidIncDist_rowsum + hbrlowIncDist_rowsum
+  hbrtrips <- hbrhighIncDist_colsum + hbrmidIncDist_colsum + hbrlowIncDist_colsum
   
   # calculate hbo trips 
   hbohighIncDist <- readMatrixOMX(in.file_tripdistribution, "hbomidIncDist")
-  hbohighIncDist_rowsum <- rowSums(hbohighIncDist)
+  hbohighIncDist_colsum <- colSums(hbohighIncDist)
   
   hbomidIncDist <- readMatrixOMX(in.file_tripdistribution, "hbomidIncDist")
-  hbomidIncDist_rowsum <- rowSums(hbomidIncDist)
+  hbomidIncDist_colsum <- colSums(hbomidIncDist)
   
   hbolowIncDist <- readMatrixOMX(in.file_tripdistribution, "hbolowIncDist")
-  hbolowIncDist_rowsum <- rowSums(hbolowIncDist)
+  hbolowIncDist_colsum <- colSums(hbolowIncDist)
   
-  hbotrips <- hbohighIncDist_rowsum + hbomidIncDist_rowsum + hbolowIncDist_rowsum
+  hbotrips <- hbohighIncDist_colsum + hbomidIncDist_colsum + hbolowIncDist_colsum
   
   # combine trips 
   TAZ <- c(1:2162)
@@ -70,13 +70,8 @@ listOMX(in.file_tripdistribution)
                lq.hbs = (hbstrips/tot.trips) / (sum(hbstrips)/sum(tot.trips)),
                lq.hbr = (hbrtrips/tot.trips) / (sum(hbrtrips)/sum(tot.trips)),
                lq.hbo = (hbotrips/tot.trips) / (sum(hbotrips)/sum(tot.trips)))
-                      
-  325/1434
-  sum(trips$hbwtrips)/sum(trips$hbwtrips+trips$hbstrips+trips$hbrtrips+trips$hbotrips)
-  (325/1434) /(sum(trips$hbwtrips)/sum(trips$hbwtrips+trips$hbstrips+trips$hbrtrips+trips$hbotrips))
-  
-  483.77049/1434.15153/((sum(trips$hbotrips)/sum(trips$hbwtrips+trips$hbstrips+trips$hbrtrips+trips$hbotrips)))
-  
+     
+
   # density plot 
   plot(density(trips$lq.hbw,na.rm=TRUE), main="HBW Trips Location Quotient")
   
@@ -199,6 +194,12 @@ listOMX(in.file_tripdistribution)
   ci.zipr[, "lq.hbrci"] <- lq.ci[,"lq.hbrci"]
   ci.zipr[, "lq.hboci"] <- lq.ci[,"lq.hboci"]
   
+  # save 
+  if (SAVE.INTERMEDIARIES) {
+    intm.file <- file.path(INTERMEDIATE_DIR, "trips.lq.centers.RData")
+    save(ci.zipr, file=intm.file)
+  }
+  
   
   # load required libraries
   library(maptools)
@@ -307,4 +308,264 @@ listOMX(in.file_tripdistribution)
   mtext("Centers based on trips lq.hboci", cex=1)
   
   saveGraph(filename=file.path(graph.path, "Centers_trips_lq_hboci"), type="pdf")
+  
+  
+  
+## Use OHAS HBO trips destination taz to identify HBO cecnters
+  
+  # Load required packages
+  require(dplyr)
+  require(tidyr)
+  
+  ## read OHAS activity, household, trip table
+  load(file.path(INPUT_DIR, "OHAS_PDX.RData"))
+  load(file.path(INPUT_DIR, "Zi.RData"))
+  
+  activity <- activity %>%
+    dplyr::select(sampn, perno, plano, thisaggact, tpurp, mode, trpdur, dtaz) %>%
+    arrange(sampn, perno, plano) %>%
+    group_by(sampn, perno) %>%
+    mutate(maxAct=n(), actNo=1:n())
+  
+  linked <- activity %>%
+    # remove the Change-Of-Mode trips (tpurp==7) in the middle of the day and
+    # the first and last 'trip'
+    filter(tpurp!=7 & actNo!=1 & actNo != maxAct) %>%
+    arrange(sampn, perno, plano) %>%
+    group_by(sampn, perno) %>%
+    mutate(actNo=1:n(), maxAct=n(),
+           lastaggact=lag(thisaggact),
+           last_tpurp=lag(tpurp) # last tpurp
+    )
+  
+  # identify home-based trips
+  #* hba (home-based all); all home-based trips
+  #* hbw (home-based work); work, not including all other activities at work and work related activities
+  #* hbs (home-based shopping); both routine and special shopping
+  #* hbr (home-based recreation); both outdoor and indoor recreation
+  #* hbo (home-based other); other home-based trips
+  linked <- mutate(linked, 
+                   h2o = ifelse(actNo!=1&(lastaggact=='Home'|lastaggact=='WorkAtHome')&(thisaggact!='Home'&thisaggact!='WorkAtHome'), 1, 0),
+                   o2h = ifelse(actNo!=1&(lastaggact!='Home'&lastaggact!='WorkAtHome')&(thisaggact=='Home'|thisaggact=='WorkAtHome'), 1, 0),
+                   hba = ifelse(h2o==1|o2h==1, 1, 0))
+  
+  
+  # identify trip purposes 
+  linked <- mutate(linked, 
+                   hbw = ifelse((h2o==1&tpurp==3)|(o2h==1&last_tpurp==3), 1, 0),
+                   hbs = ifelse((h2o==1&(tpurp==13|tpurp==14))|(o2h==1&(last_tpurp==13|last_tpurp==14)), 1, 0),
+                   hbr = ifelse((h2o==1&(tpurp==20|tpurp==21))|(o2h==1&(last_tpurp==20|last_tpurp==21)), 1, 0),
+                   hbo = ifelse((h2o==1&(tpurp!=3&tpurp!=5&tpurp!=6&tpurp!=13&tpurp!=14&tpurp!=20&tpurp!=21))
+                                |(o2h==1&(last_tpurp!=3&tpurp!=5&tpurp!=6&last_tpurp!=13&last_tpurp!=14&last_tpurp!=20&last_tpurp!=21)), 1, 0))
+  
+  # use tidyr to create a character trip purpose column tpurp.ch
+  linked <- linked %>% gather(tpurp.ch, flag, hbw:hbo) %>% filter(flag==1) %>% dplyr::select(-flag)
+  
+  linked[1:10,]
+  
+### Calculate hbo destination taz frequency 
+  
+  linkedhbo <- linked %>% 
+    filter(tpurp.ch=="hbo")  
+  
+  hbo.freq<- linkedhbo %>% 
+    group_by(dtaz) %>%
+    summarise(frequency=n())
+  
+  plot(density(hbo.freq$frequency), main="HBO Trips destination TAZ frequency")
+  
+  # Summary hbo destination taz frequency
+  summary(hbo.freq$frequency)
+  nrow(hbo.freq)
+  
+  hbo.freq.freq<- hbo.freq %>% 
+    group_by(frequency) %>%
+    summarise(freq.freq=n())
+  hbo.freq.freq[11:34,]
+  hbo.freq.freq <- as.data.frame(hbo.freq.freq)
+  # 
+  taz.hbo.freq <- merge(TAZ, hbo.freq, by.x="TAZ", by.y="dtaz", all.x=TRUE)
+  
+  
+  
+### Calculate hbw destination taz frequency 
+  
+  linkedhbw <- linked %>% 
+    filter(tpurp.ch=="hbw")  
+  
+  hbw.freq<- linkedhbw %>% 
+    group_by(dtaz) %>%
+    summarise(frequency=n())
+  
+  plot(density(hbw.freq$frequency), main="HBW Trips destination TAZ frequency")
+  
+  # Summary hbw destination taz frequency
+  summary(hbw.freq$frequency)
+  nrow(hbw.freq)
+  
+  hbw.freq.freq<- hbw.freq %>% 
+    group_by(frequency) %>%
+    summarise(freq.frqq=n())
+  
+  # 
+  taz.hbw.freq <- merge(TAZ, hbw.freq, by.x="TAZ", by.y="dtaz", all.x=TRUE)
+  
+  
+### Calculate hbs destination taz frequency 
+  
+  linkedhbs <- linked %>% 
+    filter(tpurp.ch=="hbs")  
+  
+  hbs.freq<- linkedhbs %>% 
+    group_by(dtaz) %>%
+    summarise(frequency=n())
+  
+  plot(density(hbs.freq$frequency), main="HBS Trips destination TAZ frequency")
+
+  # Summary hbs destination taz frequency
+  summary(hbs.freq$frequency)
+  nrow(hbs.freq)
+  
+  hbs.freq.freq<- hbs.freq %>% 
+    group_by(frequency) %>%
+    summarise(freq.frqq=n())
+  
+  # 
+  taz.hbs.freq <- merge(TAZ, hbs.freq, by.x="TAZ", by.y="dtaz", all.x=TRUE)
+  
+### Calculate hbr destination taz frequency 
+  
+  linkedhbr <- linked %>% 
+    filter(tpurp.ch=="hbr")  
+  
+  hbr.freq<- linkedhbr %>% 
+    group_by(dtaz) %>%
+    summarise(frequency=n())
+  
+  plot(density(hbr.freq$frequency), main="HBR Trips destination TAZ frequency")
+  
+  # Summary hbr destination taz frequency
+  summary(hbr.freq$frequency)
+  nrow(hbr.freq)
+  
+  hbr.freq.freq<- hbr.freq %>% 
+    group_by(frequency) %>%
+    summarise(freq.freq=n())
+  
+  # 
+  taz.hbr.freq <- merge(TAZ, hbr.freq, by.x="TAZ", by.y="dtaz", all.x=TRUE)
+  
+  
+  ## Map the results 
+  # read in the taz data shapefile
+  TazFile <- file.path(INPUT_DIR, "shp/TAZ.shp")
+  TazPoly <- readShapePoly(TazFile)
+  
+  # extract the attribute data
+  TazData <- TazPoly@data
+  
+  # make an index vector to the taz
+  TazIndex <- as.character(TazData$newtaz)
+  
+  # get the location of the reference zone
+  TazCentroids.ZiXy <- coordinates(TazPoly)
+  rownames(TazCentroids.ZiXy) <- TazIndex
+  
+  # write a function to plot taz values as a choropleth map
+  choropleth <- function(geo=TazPoly, data, DataIndex=TazIndex, palette="Blues", breaks,
+                         LegendSize=1, PlotRef=TRUE, LegendOffset=c(1,1),
+                         LegendTitle=NULL, RefColor="red", ...){
+    DataCut <- cut(data[DataIndex], breaks, include.lowest=TRUE, labels=FALSE)
+    ColorPalette <- brewer.pal(length(breaks)-1, palette)
+    colors <- ColorPalette[DataCut]
+    plot(geo, col=colors, xaxt="n", yaxt="n", border=NA, ...)
+    if(PlotRef) points(RefZoneCent[1], RefZoneCent[2], pch=1, col=RefColor, cex=2, lwd=2)
+    LegendText <- paste(breaks[1:(length(breaks)-1)], breaks[2:length(breaks)], sep=" - ")
+    if(LegendSize != 0){
+      legend(7404637,611959, legend=LegendText,
+             title=LegendTitle, cex=LegendSize, fill=ColorPalette)
+    }
+    if(PlotRef){
+      points(7410262, 543378, pch=1, col=RefColor, cex=2, lwd=2)
+      text(7415412, 543378, "Reference Zone", pos=4)
+    }
+  }
+  
+  
+  # Names 
+  load(file.path(INPUT_DIR, "Zi.RData"))
+  
+## Map hbo trips destination TAZ frequency
+  new.taz.hbo.freq <- taz.hbo.freq$frequency
+  names(new.taz.hbo.freq) <- Zi
+  
+  new.taz.hbo.freq[is.na(new.taz.hbo.freq)] <- 0
+  
+  # Set up plot layout
+  par(mfrow=c(1,1))
+  
+  ## map 
+  choropleth(TazPoly, new.taz.hbo.freq, TazIndex, "RdYlBu",
+             breaks=c(0,0.01,1,3,7,16,41), LegendSize=0.2, PlotRef=FALSE,
+             main="", LegendTitle="Centers")
+  
+  mtext("OHAS hbo trips destination TAZ frequency", cex=1)
+  
+  saveGraph(filename=file.path(graph.path, "Centers_trips_OHAS_hbo_DTAZ"), type="pdf")
+  
+#### Map hbw trips destination TAZ frequency
+  new.taz.hbw.freq <- taz.hbw.freq$frequency
+  names(new.taz.hbw.freq) <- Zi
+  
+  new.taz.hbw.freq[is.na(new.taz.hbw.freq)] <- 0
+  
+  # Set up plot layout
+  par(mfrow=c(1,1))
+  
+  ## map 
+  choropleth(TazPoly, new.taz.hbw.freq, TazIndex, "RdYlBu",
+             breaks=c(0,0.01,1,2,3,4,8), LegendSize=0.2, PlotRef=FALSE,
+             main="", LegendTitle="Centers")
+  
+  mtext("OHAS hbw trips destination TAZ frequency", cex=1)
+  
+  saveGraph(filename=file.path(graph.path, "Centers_trips_OHAS_hbw_DTAZ"), type="pdf")
+  
+#### Map hbs trips destination TAZ frequency
+  new.taz.hbs.freq <- taz.hbs.freq$frequency
+  names(new.taz.hbs.freq) <- Zi
+  
+  new.taz.hbs.freq[is.na(new.taz.hbs.freq)] <- 0
+  
+  # Set up plot layout
+  par(mfrow=c(1,1))
+  
+  ## map 
+  choropleth(TazPoly, new.taz.hbs.freq, TazIndex, "RdYlBu",
+             breaks=c(0,0.01,1,3,6,8,23), LegendSize=0.2, PlotRef=FALSE,
+             main="", LegendTitle="Centers")
+  
+  mtext("OHAS hbs trips destination TAZ frequency", cex=1)
+  
+  saveGraph(filename=file.path(graph.path, "Centers_trips_OHAS_hbs_DTAZ"), type="pdf")
+  
+#### Map hbr trips destination TAZ frequency
+  new.taz.hbr.freq <- taz.hbr.freq$frequency
+  names(new.taz.hbr.freq) <- Zi
+  
+  new.taz.hbr.freq[is.na(new.taz.hbr.freq)] <- 0
+  
+  # Set up plot layout
+  par(mfrow=c(1,1))
+  
+  ## map 
+  choropleth(TazPoly, new.taz.hbr.freq, TazIndex, "RdYlBu",
+             breaks=c(0,0.01,1,3,7,11,20), LegendSize=0.2, PlotRef=FALSE,
+             main="", LegendTitle="Centers")
+  
+  mtext("OHAS hbr trips destination TAZ frequency", cex=1)
+  
+  saveGraph(filename=file.path(graph.path, "Centers_trips_OHAS_hbr_DTAZ"), type="pdf")
+  
+  
   
